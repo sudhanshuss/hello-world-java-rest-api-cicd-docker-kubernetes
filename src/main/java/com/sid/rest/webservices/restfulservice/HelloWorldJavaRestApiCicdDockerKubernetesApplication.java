2cc8946c09a199209634b965a27e32dfb8723f83
shekhar, sudhanshu <sudhanshu.shekhar4@T-Mobile.com>
2019-11-17 10:16:38 -0800

    feat: inital setup

---
+++ src/main/java/com/sid/rest/webservices/restfulservice/HelloWorldJavaRestApiCicdDockerKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.sid.rest.webservices.restfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldJavaRestApiCicdDockerKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldJavaRestApiCicdDockerKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sid/rest/webservices/restfulservice/HelloWorldJavaRestApiCicdDockerKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.rest.webservices.restfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldJavaRestApiCicdDockerKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
